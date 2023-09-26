19453c78d5f30745f1198fb496135f64e7ddfad1
mikhail <mishaprizby@gmail.com>
2023-09-26 16:57:33 +0300

    Added endpoint to generate excel report for robots

--- robots/urls.py
+++ robots/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import robots_report
 
-urlpatterns = []
+
+urlpatterns = [
+    path('get_excel_report/', robots_report, name='robots_get_report')
+]
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import HttpResponse, FileResponse
+from django.views.decorators.http import require_http_methods
+import xlsxwriter
+import datetime
+from django.utils import timezone
+from .models import Robot
+from django.db.models import Count
 
-# Create your views here.
+
+@require_http_methods(["GET"])
+def robots_report(request):
+    FILE_NAME = "robots_report.xlsx"
+    COLUMNS = ["Модель", "Версия", "Количество за неделю"]
+
+    unique_models = Robot.objects.values_list("model", flat=True).distinct()
+    workbook = xlsxwriter.Workbook(FILE_NAME)
+
+    for model in unique_models:
+        sheet = workbook.add_worksheet(model)
+        for column_number in range(len(COLUMNS)):
+            sheet.write(0, column_number, COLUMNS[column_number])
+
+        versions = Robot.objects.filter(
+            created__gte=timezone.now() - datetime.timedelta(days=7), model=model
+        ).values("version").annotate(total=Count('version')).order_by('total')
+
+        for i, version in enumerate(versions):
+            sheet.write(i + 1, 0, model)
+            sheet.write(i + 1, 1, version["version"])
+            sheet.write(i + 1, 2, version["total"])
+    workbook.close()
+
+    
+    return HttpResponse(
+        FileResponse(open(FILE_NAME, "rb"),
+                     content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
